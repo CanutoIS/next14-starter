2a5d05876c0188d3656188a4fc5fde44babce96d
CanutoIS <canutosorianoivan@gmail.com>
2024-03-08 14:14:13 +0100

    add image url capability configuration

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,13 @@
+import Image from "next/image"
+import styles from './about.module.css'
+
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
+    <div>
+      <div className={styles.imgContainer}>
+        <Image src='https://images.pexels.com/photos/20232750/pexels-photo-20232750/free-photo-of-ligero-amanecer-paisaje-puesta-de-sol.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load' fill/>
+      </div>
+    </div>
   )
 }
 
