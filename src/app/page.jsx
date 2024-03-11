aab541c09350ce99f3da10213c5a72d821538f9e
CanutoIS <canutosorianoivan@gmail.com>
2024-03-11 12:50:52 +0100

    create home page design

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,24 @@
+import Image from 'next/image';
+import styles from './home.module.css'
+
 const Home = () => {
-  return <div>Homepage</div>;
+  return <div className={styles.container}>
+    <div className={styles.textContainer}>
+      <h1 className={styles.title}>Creative Thoughts Agency.</h1>
+      <p className={styles.desc}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem necessitatibus illo quo autem, cum libero sit eaque incidunt in quidem totam!</p>
+      <div className={styles.buttons}>
+        <button className={styles.button}>Learn more</button>
+        <button className={styles.button}>Contact</button>
+      </div>
+      <div className={styles.brands}>
+        <Image src="/brands.png" fill className={styles.brandImg} />
+      </div>
+    </div>
+    <div className={styles.imgContainer}>
+      <Image src="/hero.gif" fill className={styles.heroImg} />
+    </div>
+
+  </div>;
 };
 
 export default Home;
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,11 @@
+import styles from "./footer.module.css"
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={styles.container}>
+      <div className={styles.logo}>lamadev</div>
+      <div className={styles.text}></div>
+    </div>
   )
 }
 
